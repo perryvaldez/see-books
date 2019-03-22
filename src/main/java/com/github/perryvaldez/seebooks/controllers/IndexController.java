19e22f8821da4d28f09ea30b6206b72009bec689
Perry Valdez <perryv@gmail.com>
2019-03-22 11:41:44 +0800

    Added logging

--- src/main/java/com/github/perryvaldez/seebooks/controllers/IndexController.java
+++ src/main/java/com/github/perryvaldez/seebooks/controllers/IndexController.java
@@ -1,15 +1,20 @@
 package com.github.perryvaldez.seebooks.controllers;
 
+import org.apache.logging.log4j.LogManager;
+import org.apache.logging.log4j.Logger;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.ModelMap;
 import org.springframework.web.bind.annotation.GetMapping;
 
 @Controller
 public class IndexController {
+	@SuppressWarnings("unused")
+	private static final Logger LOGGER = LogManager.getLogger(IndexController.class);
+	
 	@GetMapping("/")
 	public String indexGet(ModelMap model) {	
 		model.addAttribute("myvar", "vall");
-		
+			
 		return "views/index/index";
 	}
 }
