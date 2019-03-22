5b0c09912d1d8140c2d9d2c4070a4dc269735529
Perry Valdez <perryv@gmail.com>
2019-03-22 18:30:02 +0800

    Corrected default password

--- docs/seed.sql
+++ docs/seed.sql
@@ -1,4 +1,4 @@
-insert into tbl_users (id, email, password) values (nextval('keygen'), 'admin-email@example.com', '$2a$10$AUuMXEPDkCVBBCqB5uJffuRBfF2V8yzw7eANxopPMeCaSIDB1cmW2');
+insert into tbl_users (id, email, password) values (nextval('keygen'), 'admin-email@example.com', '{bcrypt}$2a$10$Jsh2ItiYqcbdSiImhcFJ9.wniKAYOmCGNUArU25.jNhyrdyPtV6tW');
 
 insert into tbl_roles (enum, name) values (1, 'ADMIN');
 insert into tbl_roles (enum, name) values (2, 'USER');
