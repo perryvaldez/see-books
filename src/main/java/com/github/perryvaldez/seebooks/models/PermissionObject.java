a1b0faca7594b0565751639a19b38bcd60cde95a
Perry Valdez <perryv@gmail.com>
2019-03-26 14:52:21 +0800

    Defined interfaces for permission entities

---
+++ src/main/java/com/github/perryvaldez/seebooks/models/PermissionObject.java
@@ -0,0 +1,13 @@
+package com.github.perryvaldez.seebooks.models;
+
+import java.io.Serializable;
+
+import com.github.perryvaldez.seebooks.models.types.KeyType;
+
+public interface PermissionObject extends Serializable {
+    public KeyType getId();
+    public void setId(KeyType id);
+    
+    public String getName();
+    public void setName(String name);
+}
