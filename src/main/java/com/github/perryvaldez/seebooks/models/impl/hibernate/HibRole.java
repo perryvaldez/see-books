7df4f509fb2de5bc4eff91dfdf974acdd518830d
Perry Valdez <perryv@gmail.com>
2019-03-27 11:38:16 +0800

    Made the many-to-many relationship unidirectional

--- src/main/java/com/github/perryvaldez/seebooks/models/impl/hibernate/HibRole.java
+++ src/main/java/com/github/perryvaldez/seebooks/models/impl/hibernate/HibRole.java
@@ -1,15 +1,10 @@
 package com.github.perryvaldez.seebooks.models.impl.hibernate;
 
 import java.beans.Transient;
-import java.util.HashSet;
-import java.util.Set;
 
-import javax.persistence.CascadeType;
 import javax.persistence.Column;
 import javax.persistence.Entity;
-import javax.persistence.FetchType;
 import javax.persistence.Id;
-import javax.persistence.ManyToMany;
 import javax.persistence.Table;
 
 import com.github.perryvaldez.seebooks.models.Role;
@@ -57,12 +52,5 @@ public class HibRole implements Role {
 	public void setName(String name) {
 		this.name = name;
 	}
-	
-	@ManyToMany(fetch = FetchType.LAZY,
-            cascade = {
-                    CascadeType.PERSIST,
-                    CascadeType.MERGE
-                },
-            mappedBy = "roles")
-	private Set<HibUser> users = new HashSet<>();
+
 }
