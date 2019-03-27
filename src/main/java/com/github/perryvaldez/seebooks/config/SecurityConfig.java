52f0f036c7f389b2bf2928bf14d43c515d09fcb6
Perry Valdez <perryv@gmail.com>
2019-03-27 09:26:20 +0800

    Added setup for H2 Console

--- src/main/java/com/github/perryvaldez/seebooks/config/SecurityConfig.java
+++ src/main/java/com/github/perryvaldez/seebooks/config/SecurityConfig.java
@@ -1,13 +1,17 @@
 package com.github.perryvaldez.seebooks.config;
 
+import org.apache.logging.log4j.LogManager;
+import org.apache.logging.log4j.Logger;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.beans.factory.annotation.Qualifier;
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.security.config.annotation.authentication.builders.AuthenticationManagerBuilder;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
 import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
+import org.springframework.security.config.annotation.web.configurers.ExpressionUrlAuthorizationConfigurer;
 import org.springframework.security.core.userdetails.UserDetailsService;
 import org.springframework.security.crypto.factory.PasswordEncoderFactories;
 import org.springframework.security.crypto.password.PasswordEncoder;
@@ -17,12 +21,24 @@ import com.github.perryvaldez.seebooks.services.UserService;
 
 @Configuration
 @EnableWebSecurity(debug = true)
-public class SecurityConfig extends WebSecurityConfigurerAdapter {
+public class SecurityConfig extends WebSecurityConfigurerAdapter {	
+	@SuppressWarnings("unused")
+	private static final Logger LOGGER = LogManager.getLogger(SecurityConfig.class);
+	
+	@Value("${spring.datasource.url}")
+	private String dataSourceUrl;
+	
     @Override
-	protected void configure(HttpSecurity http) throws Exception {
-	    http
-	        .authorizeRequests()
-	            .antMatchers("/", "/css/*").permitAll()
+	protected void configure(HttpSecurity http) throws Exception {   	
+    	ExpressionUrlAuthorizationConfigurer<HttpSecurity>.AuthorizedUrl antMatches;
+    	
+    	if(dataSourceUrl.startsWith("jdbc:h2:mem:")) {
+    		antMatches = http.csrf().disable().authorizeRequests().antMatchers("/", "/css/*", "/h2-console/*");
+    	} else {
+    		antMatches = http.authorizeRequests().antMatchers("/", "/css/*");
+    	}
+    	
+	    antMatches.permitAll()
 	            .anyRequest().authenticated()
 	            .and()
 	        .formLogin()
