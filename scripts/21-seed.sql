b5c1238b408f9c98ebc37e43646e2c1e5567beb5
Perry Valdez <perryv@gmail.com>
2019-03-28 21:36:28 +0800

    Added new seeds

--- scripts/21-seed.sql
+++ scripts/21-seed.sql
@@ -21,19 +21,22 @@ insert into tbl_user_roles (user_id, role_id) values ((select id from tbl_users 
 insert into tbl_user_roles (user_id, role_id) values ((select id from tbl_users where email = 'user@example.com'), (select id from tbl_roles where name = 'USER'));
 
 insert into tbl_perm_actions (enum, name) values (1, '*');
-insert into tbl_perm_actions (enum, name) values (2, 'can_create');
-insert into tbl_perm_actions (enum, name) values (3, 'can_retrieve');
-insert into tbl_perm_actions (enum, name) values (4, 'can_update');
-insert into tbl_perm_actions (enum, name) values (5, 'can_delete');
-insert into tbl_perm_actions (enum, name) values (6, 'can_share');
-insert into tbl_perm_actions (enum, name) values (7, 'can_send');
-insert into tbl_perm_actions (enum, name) values (8, 'can_receive');
+insert into tbl_perm_actions (enum, name) values (2, 'can_manage');
+insert into tbl_perm_actions (enum, name) values (3, 'can_create');
+insert into tbl_perm_actions (enum, name) values (4, 'can_retrieve');
+insert into tbl_perm_actions (enum, name) values (5, 'can_update');
+insert into tbl_perm_actions (enum, name) values (6, 'can_delete');
+insert into tbl_perm_actions (enum, name) values (7, 'can_share');
+insert into tbl_perm_actions (enum, name) values (8, 'can_send');
+insert into tbl_perm_actions (enum, name) values (9, 'can_receive');
 insert into tbl_perm_actions (enum, name) values (31, 'can_execute');
 insert into tbl_perm_actions (enum, name) values (32, 'can_stop');
 insert into tbl_perm_actions (enum, name) values (33, 'can_suspend');
 insert into tbl_perm_actions (enum, name) values (34, 'can_resume');
 
 insert into tbl_perm_objects (id, name) values (nextval('keygen'), '*');
+insert into tbl_perm_objects (id, name) values (nextval('keygen'), 'user');
+insert into tbl_perm_objects (id, name) values (nextval('keygen'), 'business');
 
 insert into tbl_realms (id, name) values (nextval('keygen'), '*');
 
