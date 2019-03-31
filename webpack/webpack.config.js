a3d5d5fb0599d79bdcb9c1b03ca26118d1a83c98
Perry Valdez <perryv@gmail.com>
2019-03-31 15:14:01 +0800

    Modified webpack config

--- webpack/webpack.config.js
+++ webpack/webpack.config.js
@@ -25,68 +25,61 @@ const path = require('path');
 const UglifyJSPlugin = require('uglifyjs-webpack-plugin');
 
 module.exports = {
-	entry: path.resolve(__dirname, '../src/main/js/index.js'), 
-	module: {
-		rules: [
-			{
-				include: [path.resolve(__dirname, '../src/main/js')],
-				loader: 'babel-loader',
+  mode: 'development',
+  entry: path.resolve(__dirname, '../src/main/js/index.js'), 
+  module: {
+    rules: [
+      {
+        test: /\.js$/,
+        include: [path.resolve(__dirname, '../src/main/js')],
+        loader: 'babel-loader',
+        options: {
+          plugins: ['syntax-dynamic-import'],
+          presets: [
+            [
+              '@babel/preset-env',
+              {
+                modules: false
+              }
+            ]
+          ]
+        }
+      },
+      {
+        test: /\.css$/,
+        use: [
+          {
+            loader: 'style-loader',
+            options: {
+              sourceMap: true
+            }
+          },
+          {
+            loader: 'css-loader'
+          }
+        ]
+      }
+    ]
+  },
 
-				options: {
-					plugins: ['syntax-dynamic-import'],
+  output: {
+    chunkFilename: '[name].[chunkhash].js',
+    filename: '[name].js',
+    path: path.resolve(__dirname, '../src/main/resources/static/js')
+  },
 
-					presets: [
-						[
-							'@babel/preset-env',
-							{
-								modules: false
-							}
-						]
-					]
-				},
-
-				test: /\.js$/
-			},
-			{
-				test: /\.css$/,
-
-				use: [
-					{
-						loader: 'style-loader',
-
-						options: {
-							sourceMap: true
-						}
-					},
-					{
-						loader: 'css-loader'
-					}
-				]
-			}
-		]
-	},
-
-	output: {
-		chunkFilename: '[name].[chunkhash].js',
-		filename: '[name].[chunkhash].js',
-		path: path.resolve(__dirname, '../src/main/resources/static/js')
-	},
-
-	mode: 'development',
-
-	optimization: {
-		splitChunks: {
-			cacheGroups: {
-				vendors: {
-					priority: -10,
-					test: /[\\/]node_modules[\\/]/
-				}
-			},
-
-			chunks: 'async',
-			minChunks: 1,
-			minSize: 30000,
-			name: true
-		}
-	}
+  optimization: {
+    splitChunks: {
+      cacheGroups: {
+        vendors: {
+          priority: -10,
+          test: /[\\/]node_modules[\\/]/
+        }
+      },
+      chunks: 'async',
+      minChunks: 1,
+      minSize: 30000,
+      name: true
+    }
+  }
 };
