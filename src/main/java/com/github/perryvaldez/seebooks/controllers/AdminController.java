5a0d65ec4718b5d09b4fd3a46d5e9d293c4fe5c4
Perry Valdez <perryv@gmail.com>
2019-04-02 23:04:56 +0800

    Started processing userse

--- src/main/java/com/github/perryvaldez/seebooks/controllers/AdminController.java
+++ src/main/java/com/github/perryvaldez/seebooks/controllers/AdminController.java
@@ -2,15 +2,22 @@ package com.github.perryvaldez.seebooks.controllers;
 
 import java.util.List;
 
+import org.apache.logging.log4j.LogManager;
+import org.apache.logging.log4j.Logger;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.ModelMap;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.servlet.ModelAndView;
 
+import com.github.perryvaldez.seebooks.forms.UserForm;
 import com.github.perryvaldez.seebooks.models.types.UserWithRoles;
 import com.github.perryvaldez.seebooks.services.UserService;
 
 @Controller
 public class AdminController {
+	private static final Logger LOGGER = LogManager.getLogger(AdminController.class);
+	
     private UserService userService;
 	
 	public AdminController(UserService userService) {
@@ -23,6 +30,13 @@ public class AdminController {
 		
 		model.addAttribute("userList", userList);
 		return "views/admin/users";
+	}
+	
+	@GetMapping("/admin/users/{id}")
+	public ModelAndView usersGetById(@PathVariable String id) {
+		LOGGER.info("==== usersGetById: id = " + id);
+		
+		return new ModelAndView("views/admin/users/byid", "userForm", new UserForm());
 	}
 	
 	@GetMapping("/admin/businesses")
---
+++ src/main/java/com/github/perryvaldez/seebooks/forms/UserForm.java
@@ -0,0 +1,31 @@
+package com.github.perryvaldez.seebooks.forms;
+
+public class UserForm {
+    private String id;
+    private String email;
+    private String password;
+    
+	public String getId() {
+		return id;
+	}
+	
+	public void setId(String id) {
+		this.id = id;
+	}
+	
+	public String getEmail() {
+		return email;
+	}
+	
+	public void setEmail(String email) {
+		this.email = email;
+	}
+	
+	public String getPassword() {
+		return password;
+	}
+	
+	public void setPassword(String password) {
+		this.password = password;
+	}
+}
