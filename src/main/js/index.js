cfcd4bd42a1103d99f857130111e186ffca1a8af
Perry Valdez <perryv@gmail.com>
2019-04-10 23:55:21 +0800

    Added test Vue code

--- src/main/js/index.js
+++ src/main/js/index.js
@@ -1,9 +1,13 @@
 import Vue from 'vue';
 
-export const sample = () => 'hello, world!';
-
 export const createVue = (opts = {}) => {
   opts.delimiters = ['$[', ']'];
 
   return new Vue(opts);
+};
+
+export const createVueComponent = (name, opts = {}) => {
+  opts.delimiters = ['$[', ']'];
+  
+  return Vue.component(name, opts);
 };
