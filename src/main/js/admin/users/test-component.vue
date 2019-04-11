568d95c5d07095f9763b07876e510a879c2692fd
Perry Valdez <perryv@gmail.com>
2019-04-11 16:00:30 +0800

    Created test Vue component

---
+++ src/main/js/admin/users/test-component.vue
@@ -0,0 +1,11 @@
+<template>
+  <span>{{ message }}<input type="button" value="Test Click" v-on:click="message='Hi, again!.'" /></span>
+</template>
+
+<script>
+module.exports = {
+  data: () => ({
+    message: '',
+  }),
+}
+</script>
