fb2088c36fbc1786990d0462033ffc7352e6c5c5
Perry Valdez <perryv@gmail.com>
2019-04-11 22:30:06 +0800

    Integrated basic material dialog

--- src/main/js/admin/users/index.js
+++ src/main/js/admin/users/index.js
@@ -1,16 +1,17 @@
 import Vue from 'vue';
 import TestComponent from './test-component.vue';
+import TestDialog from './test-dialog.vue';
 
 const run = (element) => {
 	const v = new Vue({
 	  el: element,
-	  components: { TestComponent },
+	  components: { TestComponent, TestDialog },
 	  data: {
 	      message: '',
 	  },
 	  methods: {
 	    launchAddRole: (evt) => {
-	        v.message = 'Test Vue component';
+	        v.message = 'Test Vue component';	        
 	    }
 	  },
 	  delimiters: ['$[', ']'],
---
+++ src/main/js/admin/users/test-dialog.vue
@@ -0,0 +1,33 @@
+<template>
+    <md-dialog :md-active.sync="showDialog">
+      <md-dialog-title>Test Dialog</md-dialog-title>
+      <p>This is a test dialog.</p>
+      
+      <md-dialog-actions>
+        <md-button class="md-primary" @click="showDialog = false">Close</md-button>
+      </md-dialog-actions>
+    </md-dialog>
+</template>
+
+<script>
+import Vue from 'vue';
+import { MdDialog, MdDialogTitle, MdDialogActions, MdButton } from 'vue-material/dist/components';
+import 'vue-material/dist/vue-material.min.css';
+
+Vue.use(MdDialog);
+Vue.use(MdButton);
+
+export default {
+  name: 'TestDialog',
+  data: () => ({
+    showDialog: false,
+  }),
+  // components: { MdDialog, MdDialogTitle, MdDialogActions, MdButton },
+}
+</script>
+
+<style lang="scss" scoped>
+.md-dialog {
+  max-width: 768px;
+}
+</style>
